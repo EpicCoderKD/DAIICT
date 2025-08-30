314960600541343c1c1ed3af8479d5579c0dcaf7
KT-1114 <kashyapthakral1114@gmail.com>
2025-08-31 01:39:52 +0530

    added role checking

--- scripts/checkRoles.js
+++ scripts/checkRoles.js
@@ -4,7 +4,7 @@ async function main() {
   const [admin, ...others] = await hre.ethers.getSigners();
 
   // 🔹 Replace with your deployed contract address
-  const contractAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
   const contract = await hre.ethers.getContractAt("GreenH2CreditsV2", contractAddress);
 
@@ -14,7 +14,8 @@ async function main() {
     const signer = (await hre.ethers.getSigners())[i];
     const addr = signer.address;
 
-    const isAdmin = await contract.hasRole(await contract.DEFAULT_ADMIN_ROLE(), addr);
+    const DEFAULT_ADMIN_ROLE = ethers.ZeroHash;
+    const isAdmin = await contract.hasRole(DEFAULT_ADMIN_ROLE, admin.address);
     const isProducer = await contract.hasRole(await contract.PRODUCER_ROLE(), addr);
     const isAuditor = await contract.hasRole(await contract.AUDITOR_ROLE(), addr);
     const isRegulator = await contract.hasRole(await contract.REGULATOR_ROLE(), addr);
